51c749cb825e27c3601fb7ed2be8973fea7c69e7
Hyeon Jeon <hj@hcil.snu.ac.kr>
2021-05-19 17:06:23 +0900

    kde code added

---
+++ cpp/2D_Kernel_density_estimation.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <math.h>
+#include "printKDE.hpp"
+using namespace std;
+
+#define Max_Resolution 100
+#define Bandwidth 0.0001
+
+//filter with Gaussian Kernel
+void _Gaussian_filter(int resolution, int point_x, int point_y, double Pixel_info[][Max_Resolution]){
+    double sigma = 1.0;
+    double r, s = 2.0 * sigma * sigma;
+
+    for(int x = 0; x < resolution; x++){
+        for(int y = 0; y < resolution; y++){
+            double dx = x - point_x;
+            double dy = y - point_y;
+            r = sqrt(dx * dx + dy * dy);
+            double g_value = (exp(-(r * r) / s)) / (M_PI * s);
+            Pixel_info[x][y] += g_value;
+        }
+    }
+    return;
+}
+
+double (*_2D_Kernel_density_estimation(int num_point, int* point_coord[2], int num_index, int index[], float bandwidth, int resolution))[Max_Resolution]{
+    //size of point_coord is [num_point][2]
+    //size of index is [num_index]
+    static double Pixel_info[Max_Resolution][Max_Resolution] = {0, };
+    double MAX = 0;
+
+    for(int i = 0; i < num_index; i++){
+        int i_index = index[i];
+        _Gaussian_filter(resolution, point_coord[i_index][0], point_coord[i_index][1], Pixel_info);
+    }
+
+    for (int i = 0; i < resolution; i++)
+        for (int j = 0; j < resolution; j++){
+            if(Pixel_info[i][j] < bandwidth)
+                Pixel_info[i][j] = 0;
+            else
+                MAX = max(MAX, Pixel_info[i][j]);
+        }
+    if(MAX != 0){
+        for (int i = 0; i < resolution; i++)
+            for (int j = 0; j < resolution; j++)
+                Pixel_info[i][j] /= MAX;
+    }
+    return Pixel_info;
+}
+
+int main(){
+
+    int resolution;
+    int num_point;
+    int** point_coord;
+    int num_index;
+    int* index;
+
+
+    //make Input ( 일단은 num_point = num_index )
+    makeRandomInput(&resolution, &num_point, &point_coord, &num_index, &index);
+
+    // //print Randomly maked input
+    // printInput(num_point, point_coord, num_index, index);
+    //print map ( resolution * resolution )
+    printpoint(num_point, point_coord, resolution);
+
+
+    //sizeof Pixel_info : [Max_Resolution][Max_Resolution]
+    double (*Pixel_info)[Max_Resolution] ;
+    //run 2D_KDE
+    Pixel_info = _2D_Kernel_density_estimation(num_point, point_coord, num_index, index, Bandwidth, resolution);
+
+    
+    //print Pixel_info
+    printPixel_info(Pixel_info, resolution, 0.4); // Threshold = 0.4
+    
+    return 0;
+}
---
+++ cpp/printKDE.hpp
@@ -0,0 +1,88 @@
+#pragma once
+#include <iostream>
+#include <random>
+using namespace std;
+
+#define Max_Resolution 100
+#define ANSI_COLOR_RED     "\x1b[31m"
+#define ANSI_COLOR_RESET   "\x1b[0m"
+
+//print all input
+void printInput(int num_point, int** point_coord, int num_index, int* index){
+
+    cout << endl << "num_point : " << num_point << endl << "point_coord : " << endl;
+    for(int i=0;i<num_point;i++){
+        cout << point_coord[i][0] << " " << point_coord[i][1] << endl;
+    }
+    cout << endl << "num_index : " << num_index << endl << "index : ";
+    for(int i=0;i<num_index;i++)
+        cout << index[i] << " ";
+    cout << endl << endl;
+}
+
+//print point[resolution][resolution] if selected "X" else "."
+void printpoint(int num_point, int** point_coord, int resolution){
+    cout << "MAP : " << endl; 
+    int map[Max_Resolution][Max_Resolution] = {0, };
+    for(int i = 0; i < num_point; i++)
+        map[point_coord[i][0]][point_coord[i][1]] = 1;
+    for(int i = 0; i < resolution; i++){
+        for(int j = 0; j < resolution; j++)
+            if(map[i][j])
+                cout << "X ";
+            else
+                cout << ". ";
+        cout << endl;
+    }
+    cout << endl;
+}
+
+//print Pixel_info[resolution][resolution]
+void printPixel_info(double Pixel_info[][Max_Resolution], int resolution, double Threshold){
+    cout << "Pixel Info : " << endl;
+    for(int i=0;i<resolution;i++){
+        for(int j=0;j<resolution;j++)
+            if(Pixel_info[i][j] == 0)
+                printf(" .    ");
+            else if(Pixel_info[i][j] > Threshold)
+                printf(ANSI_COLOR_RED "%.3lf " ANSI_COLOR_RESET, Pixel_info[i][j]);
+            else
+                printf("%.3lf ", Pixel_info[i][j]);
+        cout << endl;
+    }
+    cout << endl;
+}
+
+//make Random Input
+void makeRandomInput(int* resolution, int* num_point, int*** point_coord, int* num_index, int** index){
+
+    random_device rd;
+    mt19937 gen(rd());
+
+    uniform_int_distribution<int> dis(1, 100);
+    //(*resolution) = dis(gen);
+    (*resolution) = 30;
+
+    uniform_int_distribution<int> dis1(0, (*resolution)*(*resolution)-1);
+    //(*num_point) = dis1(gen);
+    (*num_point) = 80;
+    (*point_coord) = (int**)malloc((*num_point) * sizeof(int*));
+    for(int i = 0; i < (*num_point); i++){
+        (*point_coord)[i] = (int*)malloc(2 * sizeof(int));
+    }
+    uniform_int_distribution<int> dis2(0, (*resolution)-1);
+    for(int i = 0; i < (*num_point); i++){
+        (*point_coord)[i][0] = dis2(gen);
+        (*point_coord)[i][1] = dis2(gen);
+    }
+
+    uniform_int_distribution<int> dis3(0, (*num_point)-1);
+    //(*num_index) = dis3(gen) + 1;
+    (*num_index) = (*num_point);
+    (*index) = (int*)malloc((*num_index)*sizeof(int));
+    for(int i = 0; i <(*num_point); i++){
+        //(*index)[i] = dis3(gen);
+        (*index)[i] = i;
+    }
+
+}
