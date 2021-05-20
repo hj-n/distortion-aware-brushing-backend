b6b4a58aa93f81f28bd71980cbf605dca7001f16
Hyeon Jeon <hj@hcil.snu.ac.kr>
2021-05-20 16:26:08 +0900

    removed unneccessary comments

--- cpp/kde.cpp
+++ cpp/kde.cpp
@@ -2,11 +2,6 @@
 #include <math.h>
 #include <chrono>
 
-// if linux
-// #include <omp.h>
-
-// if macos
-
 
 #ifdef __APPLE__
 #include "/usr/local/opt/libomp/include/omp.h"
@@ -20,12 +15,6 @@
 #include "kde.hpp"
 using namespace std;
 
-
-// float expf_fast(float a) {
-
-
-//   return u.f;
-// }
 
 //filter with Gaussian Kernel
 void _Gaussian_filter(int resolution, float bandwidth, float point_x, float point_y, float* Pixel_info){
@@ -43,6 +32,7 @@ void _Gaussian_filter(int resolution, float bandwidth, float point_x, float poin
     return;
 }
 
+// parallelized version of Kernel
 void _Gaussian_filter_parallel(int resolution, float bandwidth, float point_x, float point_y, float* Pixel_info){
     float r, s = 2.0 * bandwidth * bandwidth;
     float pi_s = s * M_PI;
@@ -65,13 +55,6 @@ void _Gaussian_filter_parallel(int resolution, float bandwidth, float point_x, f
 void _2D_Kernel_density_estimation(int num_point, float* point_coord, int num_index, int* index, float bandwidth, int resolution, float* output_pixel_info){
     float MAX = 0;
 
-
-    // chrono::steady_clock::time_point begin = chrono::steady_clock::now();
-    // chrono::steady_clock::time_point end = chrono::steady_clock::now();
-
-    // std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() << "[µs]" << std::endl;
-    /////////
-
     for(int i = 0; i < num_index; i++){
         int i_index = index[i];
         _Gaussian_filter_parallel(resolution, bandwidth, point_coord[i_index * 2], point_coord[i_index * 2 + 1], output_pixel_info);
@@ -93,6 +76,7 @@ void _2D_Kernel_density_estimation(int num_point, float* point_coord, int num_in
 }
 
 extern "C" {
+
     // output_pixel_info should be initialized as 0
     // point_coord should be pre-scaled to [0, resolution]
     void kernel_density_estimation(
@@ -106,4 +90,5 @@ extern "C" {
         float bandwidth = pow(num_index, (-1/6));
         _2D_Kernel_density_estimation(num_point, point_coord, num_index, index, bandwidth, resolution, output_pixel_info);
     }
+    
 }
